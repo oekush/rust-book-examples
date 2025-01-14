c1f237bb605b78709a52fefd57d4f16c0107cb1e
kushal thapa <thapakushal72@gmail.com>
2025-01-14 23:57:26 +0545

    plus one

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 // 
+fn main() {
+    let x = plus_one(5);
 
+    println!("The value of x is: {x}");
+}
 
-fn main(){
-    let y={
-        let x=5;x+1
-    };
-    println!("The value of y is:{y}");
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
