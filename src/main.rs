012ecafe93ae9b7b1f1b95c93f6c5b00f08f36e3
kushal thapa <thapakushal72@gmail.com>
2025-01-19 22:37:12 +0545

    f

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,6 @@
 // 
-// fn main() {
-//     let x = plus_one(5);
-
-//     println!("The value of x is: {x}");
-// }
-
-// fn plus_one(x: i32) -> i32 {
-//     x + 1
-// }
-
-
 fn main() {
-    let number = 7;
-
-    if number < 5 {
-        println!("condition was true");
-    } else {
-        println!("condition was false");
+    loop {
+        println!("again!");
     }
 }
